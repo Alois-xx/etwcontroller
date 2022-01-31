a8386ed0a9c60fa479ad2a6bf151c31624e754bb
Alois-xx <akraus1@gmx.de>
2022-01-31 08:33:46 +0100

    Update csproj to .NET SDK format but stay at .NET Framework target platform. Latest supported .NET Framework is now 4.7 which should be ok by now.

--- ETWController/Properties/AssemblyInfo.cs
+++ ETWController/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ETWController")]
-[assembly: AssemblyCopyright("Copyright © by Alois Kraus 2016")]
+[assembly: AssemblyCopyright("Copyright © by Alois Kraus 2016-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly:InternalsVisibleTo("ETWController_uTest")]
